0e00b6304171a2908fc93eac34ff607ad1f64f54
extremeheat <extreme@protonmail.ch>
2023-01-09 22:07:02 -0500

    bedrock support, add blockVariantsByStateId from prismarine-viewer

--- index.js
+++ index.js
@@ -1,138 +1,143 @@
 const mcDataToNode = require('./lib/loader')
-const cache = {} // prevent reindexing when requiring multiple time the same version
-
-function getVersion (mcVersion) {
-  if (cache[mcVersion]) { return cache[mcVersion] }
-  const mcData = data[mcVersion]
-  if (mcData == null) { return null }
-  const nmcData = mcDataToNode(mcData, mcVersion)
-  cache[mcVersion] = nmcData
-  return nmcData
-}
-
-function toMajor (version) {
-  const [a, b] = (version + '').split('.')
-  return a + '.' + b
-}
-
-function minor (version) {
-  const [, , c] = (version + '.0').split('.')
-  return parseInt(c, 10)
-}
-
-module.exports = function (mcVersion) {
-  // Check exact version first
-  let assets = getVersion(mcVersion)
-  if (assets) { return assets }
-  // If not found, resort to the last of major
-  assets = getVersion(lastOfMajor[toMajor(mcVersion)])
-  return assets
-}
-
+const path = require('path')
+const cache = { pc: {}, bedrock: {} } // prevent reindexing when requiring multiple time the same version
 const data = {
-  '1.8.8': {
-    blocksTextures: require('./minecraft-assets/data/1.8.8/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.8.8/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.8.8/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.8.8/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.8.8/blocks_models')
-  },
-  1.9: {
-    blocksTextures: require('./minecraft-assets/data/1.9/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.9/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.9/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.9/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.9/blocks_models')
-  },
-  '1.10': {
-    blocksTextures: require('./minecraft-assets/data/1.10/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.10/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.10/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.10/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.10/blocks_models')
-  },
-  '1.11.2': {
-    blocksTextures: require('./minecraft-assets/data/1.11.2/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.11.2/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.11.2/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.11.2/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.11.2/blocks_models')
-  },
-  1.12: {
-    blocksTextures: require('./minecraft-assets/data/1.12/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.12/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.12/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.12/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.12/blocks_models')
-  },
-  1.13: {
-    blocksTextures: require('./minecraft-assets/data/1.13/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.13/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.13/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.13/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.13/blocks_models')
-  },
-  '1.13.2': {
-    blocksTextures: require('./minecraft-assets/data/1.13.2/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.13.2/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.13.2/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.13.2/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.13.2/blocks_models')
-  },
-  '1.14.4': {
-    blocksTextures: require('./minecraft-assets/data/1.14.4/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.14.4/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.14.4/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.14.4/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.14.4/blocks_models')
-  },
-  '1.15.2': {
-    blocksTextures: require('./minecraft-assets/data/1.15.2/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.15.2/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.15.2/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.15.2/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.15.2/blocks_models')
-  },
-  '1.16.1': {
-    blocksTextures: require('./minecraft-assets/data/1.16.1/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.16.1/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.16.1/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.16.1/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.16.1/blocks_models')
-  },
-  '1.16.4': {
-    blocksTextures: require('./minecraft-assets/data/1.16.4/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.16.4/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.16.4/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.16.4/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.16.4/blocks_models')
-  },
-  '1.17.1': {
-    blocksTextures: require('./minecraft-assets/data/1.17.1/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.17.1/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.17.1/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.17.1/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.17.1/blocks_models')
-  },
-  '1.18.1': {
-    blocksTextures: require('./minecraft-assets/data/1.18.1/blocks_textures'),
-    itemsTextures: require('./minecraft-assets/data/1.18.1/items_textures'),
-    textureContent: require('./minecraft-assets/data/1.18.1/texture_content'),
-    blocksStates: require('./minecraft-assets/data/1.18.1/blocks_states'),
-    blocksModels: require('./minecraft-assets/data/1.18.1/blocks_models')
-  }
-}
-
-module.exports.versions = Object.keys(data)
-
-const lastOfMajor = {}
-for (const version in data) {
-  const major = toMajor(version)
-  if (lastOfMajor[major]) {
-    if (minor(lastOfMajor[major]) < minor(version)) {
-      lastOfMajor[major] = version
+  pc: {
+    '1.8.8': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.8.8/'),
+      blocksTextures: require('./minecraft-assets/data/1.8.8/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.8.8/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.8.8/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.8.8/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.8.8/blocks_models')
+    },
+    1.9: {
+      directory: path.join(__dirname, './minecraft-assets/data/1.8.9/'),
+      blocksTextures: require('./minecraft-assets/data/1.9/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.9/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.9/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.9/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.9/blocks_models')
+    },
+    '1.10': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.10/'),
+      blocksTextures: require('./minecraft-assets/data/1.10/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.10/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.10/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.10/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.10/blocks_models')
+    },
+    '1.11.2': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.11.2/'),
+      blocksTextures: require('./minecraft-assets/data/1.11.2/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.11.2/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.11.2/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.11.2/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.11.2/blocks_models')
+    },
+    1.12: {
+      directory: path.join(__dirname, './minecraft-assets/data/1.12/'),
+      blocksTextures: require('./minecraft-assets/data/1.12/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.12/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.12/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.12/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.12/blocks_models')
+    },
+    1.13: {
+      directory: path.join(__dirname, './minecraft-assets/data/1.13/'),
+      blocksTextures: require('./minecraft-assets/data/1.13/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.13/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.13/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.13/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.13/blocks_models')
+    },
+    '1.13.2': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.13.2/'),
+      blocksTextures: require('./minecraft-assets/data/1.13.2/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.13.2/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.13.2/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.13.2/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.13.2/blocks_models')
+    },
+    '1.14.4': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.14.4/'),
+      blocksTextures: require('./minecraft-assets/data/1.14.4/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.14.4/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.14.4/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.14.4/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.14.4/blocks_models')
+    },
+    '1.15.2': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.15.2/'),
+      blocksTextures: require('./minecraft-assets/data/1.15.2/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.15.2/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.15.2/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.15.2/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.15.2/blocks_models')
+    },
+    '1.16.1': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.16.1/'),
+      blocksTextures: require('./minecraft-assets/data/1.16.1/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.16.1/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.16.1/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.16.1/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.16.1/blocks_models')
+    },
+    '1.16.4': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.16.4/'),
+      blocksTextures: require('./minecraft-assets/data/1.16.4/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.16.4/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.16.4/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.16.4/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.16.4/blocks_models')
+    },
+    '1.17.1': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.17.1/'),
+      blocksTextures: require('./minecraft-assets/data/1.17.1/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.17.1/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.17.1/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.17.1/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.17.1/blocks_models')
+    },
+    '1.18.1': {
+      directory: path.join(__dirname, './minecraft-assets/data/1.18.1/'),
+      blocksTextures: require('./minecraft-assets/data/1.18.1/blocks_textures'),
+      itemsTextures: require('./minecraft-assets/data/1.18.1/items_textures'),
+      textureContent: require('./minecraft-assets/data/1.18.1/texture_content'),
+      blocksStates: require('./minecraft-assets/data/1.18.1/blocks_states'),
+      blocksModels: require('./minecraft-assets/data/1.18.1/blocks_models')
     }
-  } else {
-    lastOfMajor[major] = version
   }
 }
+
+Object.assign(data, {
+  bedrock: {
+    '1.16.220': data.pc['1.16.1'],
+    '1.17.10': data.pc['1.17.1'],
+    '1.17.40': data.pc['1.17.1'],
+    '1.18.0': data.pc['1.18.1'],
+    '1.19.1': data.pc['1.18.1']
+  }
+})
+
+const reduce = what => what.reduce((acc, cur) => {
+  const major = cur.split('.').slice(0, 2).join('.')
+  const arr = acc[major] = acc[major] || []
+  arr.push(cur)
+  return acc
+}, {})
+
+const byMajor = { pc: reduce(Object.keys(data.pc)), bedrock: reduce(Object.keys(data.bedrock)) }
+const lastOfMajor = (type, major) => byMajor[type][major][byMajor[type][major].length - 1]
+
+module.exports = function (registry) {
+  if (typeof registry === 'string') registry = require('prismarine-registry')(registry)
+  const v = registry.version
+  if (cache[v.type][v.minecraftVersion]) return cache[v.type][v.minecraftVersion]
+
+  // Check exact version first, else check the last minor version of the major
+  const assets = data[v.type][v.minecraftVersion] || data[v.type][lastOfMajor(v.type, v.majorVersion)]
+  const updated = cache[v.type][v.minecraftVersion] = mcDataToNode(assets, registry)
+  return updated
+}
+module.exports.versions = byMajor
