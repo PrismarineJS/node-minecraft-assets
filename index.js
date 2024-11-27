d7fcb2240cf223c8ad8c8c0ee14dffbdabced937
Icyadam14 <adamkahl8@gmail.com>
2024-11-27 02:18:03 -0500

    Support 1.21.1

--- index.js
+++ index.js
@@ -134,7 +134,14 @@ const data = {
     textureContent: require('./minecraft-assets/data/1.20.2/texture_content'),
     blocksStates: require('./minecraft-assets/data/1.20.2/blocks_states'),
     blocksModels: require('./minecraft-assets/data/1.20.2/blocks_models')
-  }
+  },
+  '1.21.1': {
+    blocksTextures: require('./minecraft-assets/data/1.21.1/blocks_textures'),
+    itemsTextures: require('./minecraft-assets/data/1.21.1/items_textures'),
+    textureContent: require('./minecraft-assets/data/1.21.1/texture_content'),
+    blocksStates: require('./minecraft-assets/data/1.21.1/blocks_states'),
+    blocksModels: require('./minecraft-assets/data/1.21.1/blocks_models')
+  },
 }
 
 module.exports.versions = Object.keys(data)
